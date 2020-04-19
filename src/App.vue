d3d7e652952cbc74a81451630969211f8535c4d6
Robert Gonzalez <robert_gon97@hotmail.com>
2020-04-19 10:56:32 -0400

    saving

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <menuApp id="nav" />
+    <div class="container">
+      <router-view />
     </div>
-    <router-view/>
   </div>
 </template>
-
+<script>
+  import nav from '@/components/header.vue'
+  export default {
+    components: {
+      menuApp: nav
+    }
+  }
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,17 @@
+<template>
+	<header class="header">
+		<router-link to="/">Inicio</router-link>
+		<router-link to="/categorias">Categorias</router-link>
+	</header>
+</template>
+<style>
+	.header {
+		display: inline-flex;
+	}
+	.header a {
+		margin: 5px;
+		color: #42b983;
+		text-transform: uppercase;
+		font-weight: bold;
+	}
+</style>
