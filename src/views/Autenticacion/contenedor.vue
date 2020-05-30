32d2466530ba9c357debd2ca749d5bd5102b7eee
Deni Aguirre <deniaguirre@gmail.com>
2020-05-30 16:25:20 -0400

    Fix view

--- src/views/Autenticacion/contenedor.vue
+++ src/views/Autenticacion/contenedor.vue
@@ -1,15 +1,16 @@
 <template>
-  <transition mode="in-out">
+  <transition mode="slide">
     <div class="d-flex justify-content-center">
       <div id="visualizador" class="col-md-10 mb-3">
         <div class="imagen my-3">
           <img src="./../../assets/Imagenes/logo.jpg" alt="Ivelc" class="img-fluid" />
         </div>
-        <transition mode="in-out">
-          <router-view />
-        </transition>
+        <router-view />
+        <br class="my-5 py-5" />
+        <br class="my-5 py-5" />
+        <br class="my-5 py-5" />
         <footer>
-          <nav class="navbar fixed-bottom justify-content-center p-0 bg-white">
+          <nav class="navbar fixed-bottom justify-content-center p-2 bg-white">
             <p class="text-center"><b>Ivelk Copyright @2020 - Todos los derechos reservados</b></p>
           </nav>
         </footer>
