37b5aa5c9315a5c2c5eeceffe092d389b318b90d
Deni Aguirre <deniaguirre@gmail.com>
2020-05-30 15:03:47 -0400

    Adding login base

--- src/views/Autenticacion/login.vue
+++ src/views/Autenticacion/login.vue
@@ -6,6 +6,34 @@
           <h3>Iniciar Sesión</h3>
         </div>
       </div>
+      <div class="d-flex flex-wrap justify-content-center">
+        <div class="col-12 col-md-7 mb-3">
+          <el-input placeholder="Usuario" type="email" autocomplete="username" clearable v-model="login.user" />
+        </div>
+        <div class="col-12 col-md-7 mb-3">
+          <el-input placeholder="Contraseña" type="password" autocomplete="password" clearable show-password v-model="login.password" />
+        </div>
+        <div class="col-12 col-md-7 mb-3 text-center">
+          <hr />
+          <el-button button="submit" class="btn-primario" round>Iniciar Sesion</el-button>
+        </div>
+        <div class="col-12 text-center">
+          <p class="m-0 p-0">No tienes usuario? <router-link to="/autenticador/registro">Regístrate!</router-link> </p>
+          <p class="m-0 p-0">Olvidastes tus datos? <router-link to="/autenticador/recuperar">Recupera tu usuario!</router-link> </p>
+        </div>
+      </div>
     </form>
   </div>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        login: {
+          user: null,
+          password: null
+        }
+      }
+    }
+  }
+</script>
