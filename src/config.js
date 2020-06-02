88e74669eb006a96877fc1a251ab6b202ec07b12
Robert Gonzalez <robert_gon97@hotmail.com>
2020-06-02 19:35:52 -0400

    Importing axios glogablly

---
+++ src/config.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const backend = {
+  url: ``
+}
+
+const frontend = {
+  titulo: `Ivelk`,
+  notificacion: {
+    titulo: `Ivelk`
+  }
+}
+
+module.exports = {
+  backend,
+  frontend
+}
--- src/main.js
+++ src/main.js
@@ -1,20 +1,29 @@
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Componentsglobals from './globalComponents'
+import Config from './config'
 import Element from 'element-ui'
 import Locale from 'element-ui/lib/locale/lang/es'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import Meta from 'vue-meta'
+import VueAxios from 'vue-axios'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
+Axios.defaults.baseURL = Config.backend.url
+Axios.defaults.headers = {
+  'Content-Type': 'application/json',
+  Authorization: localStorage.token_ivelk ? `Bearer ${localStorage.token_ivelk}` : null
+}
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(Element, { Locale })
 Vue.use(Meta)
 Vue.use(Componentsglobals)
+Vue.use(VueAxios, Axios)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
