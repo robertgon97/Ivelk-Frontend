0275f66ee7d858b7f19d4b0591f3d048f0e5d436
Deni Aguirre <deniaguirre@gmail.com>
2020-06-14 13:06:07 -0400

    Adding menu

---
+++ src/views/Menu/MenuSystem.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <el-menu class="el-menu-vertical-demo" :router="true" :default-active="$route.path" mode="vertical" :collapse="!true">
+      <router-link to="/tienda" class="no-underline">
+        <div class="d-flex flex-wrap p-2 justify-content-center align-items-center">
+          <el-avatar :size="60" src="@/assets/logo.png" class="my-2">
+            <img src="@/assets/logo.png"/>
+          </el-avatar>
+          <h5 class="text-center mt-2 mx-2">{{titleApp || 'Sin título'}}</h5>
+        </div>
+      </router-link>
+      <hr />
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-user"></i>
+          <span>Perfil</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item>Modificar mi perfil</el-menu-item>
+          <el-menu-item>Cerrar Sesión</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <hr />
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-user"></i>
+          <span>Usuarios</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item>Lista de Usuarios</el-menu-item>
+          <el-menu-item>Crear de Usuarios</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-goods"></i>
+          <span>Artículos</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item>Lista de Artículos</el-menu-item>
+          <el-menu-item>Crear Artículo</el-menu-item>
+          <el-menu-item>Devoluciones</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-document-copy"></i>
+          <span>Proveedores</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item>Lista de Proveedores</el-menu-item>
+          <el-menu-item>Añadir Proveedores</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="5">
+        <template slot="title">
+          <i class="el-icon-sold-out"></i>
+          <span>Compras</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item>Lista de Compras</el-menu-item>
+          <el-menu-item>Crear Orden</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="6">
+        <template slot="title">
+          <i class="el-icon-shopping-cart-full"></i>
+          <span>Ventas</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item>Todas las Ventas</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="7">
+        <template slot="title">
+          <i class="el-icon-coin"></i>
+          <span>Balances</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item>Balances Generales</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="99">
+        <template slot="title">
+          <i class="el-icon-setting"></i>
+          <span>Configuración</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item>Configuración General</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </div>
+</template>
+<script>
+  import config from './../../config'
+  export default {
+    data () {
+      return {
+        uploading: false,
+        titleApp: config.frontend.titulo
+      }
+    }
+  }
+</script>
+<style scoped>
+  .no-underline {
+    text-decoration: none;
+  }
+</style>
---
+++ src/views/Menu/MenuView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row m-0 p-p justify-content-between">
+    <div class="col-md-2 p-0">
+      <menu-ivelk-admin />
+    </div>
+    <div class="col-md-10 p-0">
+      <div class="bg-light p-3"><h5 class="text-center m-0 p-0">{{$route.name || 'Sin Título'}}</h5></div>
+      <div class="p-3">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import config from './../../config'
+  import menu from './MenuSystem.vue'
+  export default {
+    metaInfo: {
+      titleTemplate: '%s | Menú'
+    },
+    components: {
+      'menu-ivelk-admin': menu
+    },
+    data () {
+      return {
+        uploading: false,
+        titleApp: config.frontend.titulo
+      }
+    }
+  }
+</script>
