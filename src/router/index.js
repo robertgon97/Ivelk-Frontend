949a19a600b3dbf4b57fea82c739e0398478e4ac
Robert Gonzalez <robert_gon97@hotmail.com>
2020-07-11 18:17:13 -0400

    Fix scroll

--- src/router/index.js
+++ src/router/index.js
@@ -311,6 +311,14 @@ const router = new VueRouter({
   mode: 'history',
   linkActiveClass: 'active is-active',
   routes,
+  scrollBehavior (to) {
+    if (to.hash) {
+      return {
+        selector: to.hash
+      }
+    }
+    else return { x: 0, y: 0 }
+  }
 })
 
 router.beforeEach((to, from, next) => {
