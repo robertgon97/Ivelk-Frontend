d0fee3b1d32c053810f9a73b6456d8cde1146f7c
Robert Gonzalez <robert_gon97@hotmail.com>
2020-07-12 12:33:43 -0400

    Fix notificacion

--- src/views/Menu/MenuViewAdmin.vue
+++ src/views/Menu/MenuViewAdmin.vue
@@ -4,7 +4,19 @@
       <menu-ivelk-admin />
     </div>
     <div class="col-md-10 col-sm-9 p-0 overflow-auto">
-      <div id="menucito" class="bg-light p-3"><h5 class="text-center m-0 p-0">{{$route.name || 'Sin Título'}}</h5></div>
+      <div id="menucito" class="bg-light p-3 d-flex flex-wrap justify-content-between">
+        <h5 class="text-center m-0 p-0">{{$route.name || 'Sin Título'}}</h5>
+        <el-dropdown>
+          <span class="el-dropdown-link text-light">
+            <i class="el-icon-bell"></i> {{getAllSummary.alertaNiveles.length}} Notificaciones<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item v-for="(not, index) in getAllSummary.alertaNiveles" :key="index">
+              <router-link :to="`/tienda/articulos/${not.articulos_id}`">Alertas de niveles {{(getgetAppConfig.config_inventario_minimo >= not.stock_cantidad) ? 'Bajos' : 'Altos'}} del artículo {{not.articulos_nombres}}</router-link>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
       <div class="p-3">
         <router-view />
       </div>
@@ -41,6 +53,14 @@
       return {
         uploading: false,
         titleApp: config.frontend.titulo
+      }
+    },
+    computed: {
+      getAllSummary () {
+        return this.$store.getters.getAllSummary
+      },
+      getgetAppConfig () {
+        return this.$store.getters.getgetAppConfig
       }
     }
   }
